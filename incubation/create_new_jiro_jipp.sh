5745e5d78f52fb6dec85ec5fcdd250e8e36bc30b
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2019-03-06 19:19:07 +0100

    Fix relative paths and variable name in create_new_jiro_jipp.sh script

--- incubation/create_new_jiro_jipp.sh
+++ incubation/create_new_jiro_jipp.sh
@@ -8,7 +8,7 @@ set -o pipefail
 IFS=$'\n\t'
 script_name="$(basename ${0})"
 
-ci-admin_dir="../ci-admin"
+ci_admin_dir="../../ci-admin"
 
 project_name="${1:-}"
 display_name="${2:-}"
@@ -41,13 +41,14 @@ if [[ "$project_name" != *.* ]]; then
   exit 1
 fi
 
-./jenkins-new-instance.sh ${project_name} ${display_name}
-pushd ${ci-admin_dir}
+../jenkins-new-instance.sh ${project_name} ${display_name}
+pushd ${ci_admin_dir}
 ./add_creds_gerrit.sh ${project_name} || true # if gerrit creds already exist, ignore exit code 1
 popd
 oc create namespace ${short_name}
-./secrets/create_gerrit_ssh_keys_secret.sh ${project_name}
+../secrets/create_gerrit_ssh_keys_secret.sh ${project_name}
+pushd ../
 make deploy_${project_name}
-
+popd
 
 
