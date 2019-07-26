4e51bd8f9982a20990202dc9199e1ed73a741cbc
Mikaël Barbero <mikael.barbero@gmail.com>
2019-07-26 17:51:21 +0200

    Set bash script mode to switch-maitenance script

--- jenkins-switch-maintenance.sh
+++ jenkins-switch-maintenance.sh
@@ -1,4 +1,19 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
+#*******************************************************************************
+# Copyright (c) 2018 Eclipse Foundation and others.
+# This program and the accompanying materials are made available
+# under the terms of the Eclipse Public License 2.0
+# which is available at http://www.eclipse.org/legal/epl-v20.html,
+# or the MIT License which is available at https://opensource.org/licenses/MIT.
+# SPDX-License-Identifier: EPL-2.0 OR MIT
+#*******************************************************************************
+
+# Bash strict-mode
+set -o errexit
+set -o nounset
+set -o pipefail
+
+IFS=$'\n\t'
 
 maintenance_ns="${maintenance_ns:-"error-pages"}"
 maintenance_service="${maintenance_service:-maintenance-sign}"
