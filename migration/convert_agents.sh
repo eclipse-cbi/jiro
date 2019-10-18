ce110bf70de1207b3fd97d10c57b41f1ebb8f77a
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2019-10-18 14:38:32 +0200

    Improve convert_agents.sh script
    Fix keyscan, fix escaping isse (use single quotes), improve
    create_node_properties

--- migration/convert_agents.sh
+++ migration/convert_agents.sh
@@ -19,6 +19,13 @@ short_name="${1:-}"
 temp_yaml_name="temp.yml"
 result_yaml_name="nodes.yml"
 
+if [[ -z "${short_name}" ]]; then
+  printf "ERROR: short name must not be empty\n"
+  exit 1
+fi
+
+printf "Short name: ${short_name}\n"
+
 # the executable name is different on some OSes (e.g. it's 'xml' on SuSE)
 xmlstarlet_bin=xmlstarlet
 
@@ -66,13 +73,17 @@ create_launcher() {
     #TODO: jvmOptions
 
     #use ssh-keyscan to get host key
-    #only works for external slaves
-    #key=$(ssh-keyscan ${host} | grep ssh-rsa)
-    #only works for internal slaves
-    #TODO: more than one line!?
-    #key=$(ssh -C foundation "ssh-keyscan centslave2.eclipse.org | grep ssh-rsa")
-    #TODO: remove host from beginning of line
-    key="ssh-rsa FIXME"
+    if [[ ${host} == *".eclipse.org"* ]]; then
+      #only works for internal agents
+      #TODO: more than one line!?
+      key=$(ssh -C foundation "ssh-keyscan ${host} | grep ssh-rsa")
+    else
+      #only works for external agents
+      key=$(ssh-keyscan ${host} | grep ssh-rsa)
+    fi
+
+    #remove host from beginning of line
+    key=${key/${host} /}
     cat <<EOJ  >> ${temp_yaml_name}
       launcher:
         ssh:
@@ -92,32 +103,77 @@ EOJ
     cat <<EOK  >> ${temp_yaml_name}
       launcher:
         command:
-          command: "${command}"
+          command: '${command}'
 EOK
+  elif [[ ${class} == "hudson.slaves.JNLPLauncher" ]]; then
+    tunnel=$(xmlstarlet_wrapper sel -T -t -c "/slave/launcher/tunnel" ${filename})
+    
+    disabled=$(xmlstarlet_wrapper sel -T -t -c "/slave/launcher/workDirSettings/disabled" ${filename})
+    internalDir=$(xmlstarlet_wrapper sel -T -t -c "/slave/launcher/workDirSettings/internalDir" ${filename})
+    failIfWorkDirIsMissing=$(xmlstarlet_wrapper sel -T -t -c "/slave/launcher/workDirSettings/failIfWorkDirIsMissing" ${filename})
+    #TODO: workDirPath
+
+    cat <<EOL  >> ${temp_yaml_name}
+      launcher:
+        jnlp:
+          tunnel: "${tunnel}"
+          workDirSettings:
+            disabled: ${disabled}
+            internalDir: "${internalDir}"
+            failIfWorkDirIsMissing: ${failIfWorkDirIsMissing}
+EOL
   fi
 }
 
-create_env_variables() {
+create_node_properties() {
   local filename=$1
-  count=$(xmlstarlet_wrapper sel -t -v "count(//envVars/tree-map/string)" ${filename})
+  env_count=$(xmlstarlet_wrapper sel -t -v "count(//envVars/tree-map/string)" ${filename})
+  tools_count=$(xmlstarlet_wrapper sel -t -v "count(//hudson.tools.ToolLocationNodeProperty/locations/hudson.tools.ToolLocationNodeProperty_-ToolLocation)" ${filename})
 
-  if [[ ${count} > 0 ]]; then
-    cat <<EOH  >> ${temp_yaml_name}
+  if [[ ${env_count} > 0 || ${tools_count} > 0 ]]; then
+    cat <<EOM  >> ${temp_yaml_name}
       nodeProperties:
+EOM
+fi
+
+  # Env variables
+  if [[ ${env_count} > 0 ]]; then
+    cat <<EOH  >> ${temp_yaml_name}
       - envVars:
           env:
 EOH
-    for (( i=1; i < ${count}; i++ ))
+    for (( i=1; i < ${env_count}; i++ ))
     do
       env_key=$(xmlstarlet_wrapper sel -T -t -c "//envVars/tree-map/string[${i}]" ${filename})
       i=$((i+1))
       env_value=$(xmlstarlet_wrapper sel -T -t -c "//envVars/tree-map/string[${i}]" ${filename})
       cat <<EOG  >> ${temp_yaml_name}
           - key: "${env_key}"
-            value: "${env_value}"
+            value: '${env_value}'
 EOG
     done
   fi
+
+  # Tool Locations
+  if [[ ${tools_count} > 0 ]]; then
+    cat <<EON  >> ${temp_yaml_name}
+      - toolLocation:
+          locations:
+EON
+    for (( i=1; i <= ${tools_count}; i++ ))
+    do
+      type=$(xmlstarlet_wrapper sel -T -t -c "////hudson.tools.ToolLocationNodeProperty/locations/hudson.tools.ToolLocationNodeProperty_-ToolLocation[${i}]/type" ${filename})
+      name=$(xmlstarlet_wrapper sel -T -t -c "////hudson.tools.ToolLocationNodeProperty/locations/hudson.tools.ToolLocationNodeProperty_-ToolLocation[${i}]/name" ${filename})
+      home=$(xmlstarlet_wrapper sel -T -t -c "////hudson.tools.ToolLocationNodeProperty/locations/hudson.tools.ToolLocationNodeProperty_-ToolLocation[${i}]/home" ${filename})
+      cat <<EOO  >> ${temp_yaml_name}
+          - home: '${home}'
+            key: "${type}@${name}"
+EOO
+    done
+  fi
+
+# TODO: "Disable Xvnc execution on this node" (not supported by JCasC yet?)
+
 }
 
 read_xml() {
@@ -127,12 +183,11 @@ read_xml() {
   label=$(xmlstarlet_wrapper sel -T -t -c "/slave/label" ${filename})
   remotefs=$(xmlstarlet_wrapper sel -T -t -c "/slave/remoteFS" ${filename})
   executors=$(xmlstarlet_wrapper sel -T -t -c "/slave/numExecutors" ${filename})
-
-  create_node ${name} ${description} ${label} ${remotefs} ${executors}
+  create_node "${name}" "${description}" "${label}" "${remotefs}" "${executors}"
  
   create_launcher ${filename}
 
-  create_env_variables ${filename}
+  create_node_properties ${filename}
   
   cat ${temp_yaml_name}
 }
