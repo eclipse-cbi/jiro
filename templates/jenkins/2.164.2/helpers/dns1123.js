86e55ee85f775e5ecd5f6eeaa20c43787f872c28
Mikaël Barbero <mikael.barbero@gmail.com>
2020-03-26 18:53:25 +0100

    Removed old unsupported Jenkins version tempaltes

--- templates/jenkins/2.164.2/helpers/dns1123.js
+++
@@ -1,9 +0,0 @@
-var dns1123 = function () {};
-
-dns1123.register = function (Handlebars) {
-  Handlebars.registerHelper('dns1123', function(s, options) {
-    return s.toLowerCase().replace(/^[^a-z0-9]/, 'jiro-').replace(/[^-a-z0-9]/g, '-').replace(/[^a-z0-9]$/, '');
-  });
-};
-
-module.exports = dns1123;
