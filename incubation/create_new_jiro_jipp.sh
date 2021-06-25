7d43cd4d0b381549b72074073d94d5ec7124a894
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2021-06-25 21:05:38 +0200

    Remove migration related code, fix most shellcheck warnings

--- incubation/create_new_jiro_jipp.sh
+++ incubation/create_new_jiro_jipp.sh
@@ -6,16 +6,15 @@ set -o nounset
 set -o pipefail
 
 IFS=$'\n\t'
-script_name="$(basename ${BASH_SOURCE[0]})"
-script_folder="$(dirname $(readlink -f "${BASH_SOURCE[0]}"))"
+script_name="$(basename "${BASH_SOURCE[]}")"
+script_folder="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 ci_admin_dir="${script_folder}/../../ci-admin"
 
 project_name="${1:-}"
 display_name="${2:-}"
 short_name=${project_name##*.}
-default_host="ci.eclipse.org"
-migration_host="ci-staging.eclipse.org"
+hostname="ci.eclipse.org"
 
 usage() {
   printf "Usage: %s project_name display_name\n" "$script_name"
@@ -43,57 +42,22 @@ if [[ "$project_name" != *.* ]]; then
   read -p "Press enter to continue or CTRL-C to stop the script"
 fi
 
-new_migration_instance() {
-  mkdir -p "${script_folder}/../instances/${project_name}"
-  cat <<EOF > "${script_folder}/../instances/${project_name}/config.jsonnet"
-{
-  project+: {
-    fullName: "${project_name}",
-    displayName: "${display_name}",
-  },
-  deployment+: {
-    host: "ci-staging.eclipse.org",
-    cluster: "okd-c1",
-  }
-}
-EOF
-
-  cat <<EOG > "${script_folder}/../instances/${project_name}/jiro.jsonnet"
-local jiro = import '../../templates/jiro.libsonnet';
-
-jiro+ {
-  "config.json"+: import "config.jsonnet",
-}
-EOG
- 
-}
-
-migration_or_not() {
-  read -p "Is this a migration from the old infrastructure? (Y)es, (N)o, E(x)it: " yn
-  case $yn in
-    [Yy]* ) new_migration_instance;hostname=${migration_host};;
-    [Nn]* ) ${script_folder}/../jenkins-new-instance.sh ${project_name} ${display_name};hostname=${default_host};;
-    [Xx]* ) exit;;
-        * ) echo "Please answer (Y)es, (N)ooo, E(x)it";;
-  esac
-}
-
 provisioning() {
-  pushd ${ci_admin_dir}
-  ./add_creds_gerrit.sh ${project_name} || : # if creds already exist, ignore exit code 1
-  ./add_creds_projects-storage.sh ${project_name} || : # if creds already exist, ignore exit code 1
+  pushd "${ci_admin_dir}"
+  ./add_creds_gerrit.sh "${project_name}" || : # if creds already exist, ignore exit code 1
+  ./add_creds_projects-storage.sh "${project_name}" || : # if creds already exist, ignore exit code 1
   popd
   if [[ $(oc get projects | grep -e "^${short_name} ") ]]; then
-    printf "Namespace ${project_name} already exists. Skipping creation...\n"
+    printf "Namespace %s already exists. Skipping creation...\n" "${project_name}"
   else
-    oc create namespace ${short_name}
+    oc create namespace "${short_name}"
   fi
-  ${script_folder}/../secrets/create_gerrit_ssh_keys_secret.sh ${project_name}
-  make -C ${script_folder}/.. deploy_${project_name}
+  "${script_folder}/../secrets/create_gerrit_ssh_keys_secret.sh" "${project_name}"
+  make -C "${script_folder}/.." "deploy_${project_name}"
 }
 
 wait_for_jipp_post_setup() {
-  local host=$1
+  local host="${1:-}"
   printf "Waiting for JIPP to come online..."
   echo "host: ${host}"
   n=0
@@ -101,19 +65,19 @@ wait_for_jipp_post_setup() {
   do
     curl -sL -w "%{http_code}\n" "https://${host}/${short_name}" -o /dev/null | grep 200 && break
     printf "."
-    n=$[$n+1]
+    n=$((n+1))
     sleep 12
   done
   printf "\n"
   
   if [[ $(curl -sL -w "%{http_code}\n" "https://${host}/${short_name}" -o /dev/null | grep 200) ]]; then
     printf "JIPP is online!\n"
-    ./post_setup.sh ${project_name}
+    ./post_setup.sh "${project_name}"
   else
     printf "ERROR: JIPP is not online after three minutes, please investigate and run post_setup.sh manually!\n"
   fi
 }
 
-migration_or_not
+"${script_folder}/../jenkins-new-instance.sh" "${project_name}" "${display_name}"
 provisioning
-wait_for_jipp_post_setup ${hostname}
+wait_for_jipp_post_setup "${hostname}"
