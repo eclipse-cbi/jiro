27939e7ad1e6e9eebf73b0d3058b176fe3dfa8cf
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2022-05-16 17:38:54 +0200

    Add script to check for "queueItemAuthenticator" race condition

---
+++ incubation/check_for_error.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+#*******************************************************************************
+# Copyright (c) 2022 Eclipse Foundation and others.
+# This program and the accompanying materials are made available
+# under the terms of the Eclipse Public License 2.0
+# which is available at http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+#*******************************************************************************
+
+# Check for "queueItemAuthenticator" JCasC race condition(?)
+
+# Bash strict-mode
+#set -o errexit
+set -o nounset
+set -o pipefail
+
+IFS=$'\n\t'
+#script_name="$(basename "${BASH_SOURCE[0]}")"
+#SCRIPT_FOLDER="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+CI_HOST="https://ci.eclipse.org"
+
+if [[ "${#}" -eq 0 ]]; then
+  echo "ERROR: you must provide at least one 'instance' path"
+  exit 1
+fi
+
+#no_of_processes=4
+for instance in "${@}"; do
+  #while [ $(jobs | wc -l) -ge ${no_of_processes} ] ; do sleep 1 ; done
+  project_name="$(basename "${instance}")"
+  printf "%s: " "${project_name}"
+  short_name="${project_name##*.}"
+
+  if [[ "${short_name}" == "webdev" ]] || [[ "${short_name}" == "infra" ]]; then
+    CI_HOST="https://foundation.eclipse.org/ci"
+  fi
+
+  #TODO: wrap with &
+
+  if curl -SsL "${CI_HOST}/${short_name}" | grep "queueItemAuthenticator"; then
+    echo "ERROR"
+  else
+    echo "OK"
+  fi
+done
