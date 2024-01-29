db37aa93a25b381eaff7c961fc2c826a8b15058c
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2024-01-29 16:19:32 +0100

    Remove Jenkins 2.190.1 specific templates

--- templates/jenkins/2.190.1/helpers/ifeq.js
+++
@@ -1,9 +0,0 @@
-var ifeq = function () {};
-
-ifeq.register = function (Handlebars) {
-  Handlebars.registerHelper('ifeq', function(a, b, options) {
-    return (a == b) ? options.fn(this) : options.inverse(this);
-  });
-};
-
-module.exports = ifeq;
