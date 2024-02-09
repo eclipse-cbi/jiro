297b32b6577adfe76685b905baa89883276ce7fc
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2024-02-09 20:13:59 +0100

    Switch TOOLS_IMAGE to eclipse-temurin

--- build/build-image.sh
+++ build/build-image.sh
@@ -33,7 +33,7 @@ fi
 
 "${SCRIPT_FOLDER}/gen-jenkins.sh" "${INSTANCE}"
 
-TOOLS_IMAGE="eclipsecbi/adoptopenjdk-coreutils:openjdk11-openj9-debian-slim"
+TOOLS_IMAGE="eclipsecbi/eclipse-temurin-coreutils:11-alpine"
 CONFIG_JSON="${INSTANCE}/target/config.json"
 
 install_additional_plugins() {
