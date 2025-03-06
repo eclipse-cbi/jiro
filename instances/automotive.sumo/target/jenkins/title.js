c511c1bf62c2c7fb922b79ad13e3f7f7924362ae
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2025-03-06 21:29:43 +0100

    Upgrade to latest Jenkins LTS release 2.492.2

--- instances/automotive.sumo/target/jenkins/title.js
+++ instances/automotive.sumo/target/jenkins/title.js
@@ -1,1 +1,15 @@
 document.title = "Eclipse SUMO - " + document.title;
+document.addEventListener('DOMContentLoaded', function() {
+    let header = document.querySelector('.page-header__brand');
+    if (header) {
+        let newLink = document.createElement('a');
+        newLink.href = 'https://github.com/eclipse-cbi/jiro/blob/master/instances/automotive.sumo/target/config.json';
+        newLink.textContent = 'JCasC Source';
+        newLink.style = 'color: white; border-left: 1px solid white; padding-left: 1em; font-size: 1.1em; position: relative; top: 0.2em; left: -1.6em;';
+        newLink.target = '_blank';
+        newLink.title = 'JIRO JCasC Configuration as Code';
+        header.appendChild(newLink);
+    } else {
+        console.log('Element with class "header" not found.');
+    }
+});
