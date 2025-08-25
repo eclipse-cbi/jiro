fb995ef061a7abd359d04435127cad436db344c0
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2025-08-25 14:35:56 +0200

    Improve retire_jipp.sh script
    - push changes to JIRO repo
    - remove credentials from pass
    - update list of JIPPs on ci.eclipse.org dashboard
    - update prometheus scrape targets

--- incubation/retire_jipp.sh
+++ incubation/retire_jipp.sh
@@ -43,6 +43,8 @@ FILE_SERVER_USER="$("${SCRIPT_FOLDER}/../utils/local_config.sh" "get_var" "user"
 FILE_SERVER_PW="$("${SCRIPT_FOLDER}/../utils/local_config.sh" "get_var" "pw" "file_server")"
 FILE_SERVER_PW_ROOT="$("${SCRIPT_FOLDER}/../utils/local_config.sh" "get_var" "pw_root" "file_server")"
 
+CBI_PASS_DIR="$("${SCRIPT_FOLDER}/../utils/local_config.sh" "get_var" "cbi-dir" "password-store")"
+
 create_retire_script() {
   local backup_file_name="${BACKUP_FILE_NAME}"
   local jenkins_home="/var/jenkins"
@@ -173,6 +175,91 @@ delete_project() {
   rm -rf "../instances/${PROJECT_NAME}"
 }
 
+#git_commit_push() {
+  #TODO: add method
+#}
+
+commit_jiro_changes() {
+  local JIRO_DEFAULT_BRANCH="master"
+  echo
+  echo "Commiting jiro changes..."
+  # TODO: make sure that the git index is empty
+  # TODO: deal with case where files have already been added
+  git add "../instances/${PROJECT_NAME}"
+  #TODO: diff index
+  if git diff --staged --exit-code > /dev/null; then
+    echo "INFO: Found no differences, skipping commit..."
+    return
+  else
+    #TODO: diff index
+    git diff --staged --compact-summary
+  fi
+  read -rsp $'Press enter to continue or CTRL-C to stop the script...\n'
+  echo
+  git commit -m "Retire ${PROJECT_NAME}"
+  git log -1
+  read -rsp $'Press enter to continue or CTRL-C to stop the script...\n'
+  git push origin "${JIRO_DEFAULT_BRANCH}"
+}
+
+delete_pass_credentials() {
+  local CBI_PASS_DEFAULT_BRANCH="master"
+  # TODO: make sure that the git index is empty
+  # TODO: pull before push
+  pushd "${CBI_PASS_DIR}" > /dev/null
+    git rm -r "${CBI_PASS_DIR}/bots/${PROJECT_NAME}"
+    if git diff --staged --exit-code > /dev/null; then
+      echo "INFO: Found no differences, skipping commit..."
+      popd > /dev/null
+      return
+    else
+      #TODO: diff index
+      git diff --staged --compact-summary
+    fi
+    read -rsp $'Press enter to continue or CTRL-C to stop the script...\n'
+    echo
+    git commit -m "Retire ${PROJECT_NAME}"
+    git log -1
+    read -rsp $'Press enter to continue or CTRL-C to stop the script...\n'
+    git push origin "${CBI_PASS_DEFAULT_BRANCH}"
+  popd > /dev/null
+}
+
+update_list_of_jipps() {
+  echo "Updating the list of JIPPs on ci.eclipse.org..."
+  local ci_dashboard_folder="${SCRIPT_FOLDER}/../../ci.eclipse.org-dashboard"
+  #check if ci.eclipse.org-dashboard repo is available
+  if [[ ! -d "${ci_dashboard_folder}" ]]; then
+    echo "ci.eclipse.org-dashboard git repo not found! It is expected to exist here: ${ci_dashboard_folder}"
+    echo "Clone the ci.eclipse.org-dashboard git repo and try again!"
+    #TODO: offer checkout
+    return
+  fi
+  pushd "${ci_dashboard_folder}" > /dev/null
+    ./build_site.sh
+    echo "TODO: Commit and push changes in ${ci_dashboard_folder}"
+    #git_commit_push "${ci_dashboard_folder}"
+  popd > /dev/null
+  # double-check that the list was updated
+}
+
+update_prometheus_scrape_targets() {
+  local prometheus_folder="${SCRIPT_FOLDER}/../../prometheus"
+  #check if prometheus repo is available
+  if [[ ! -d "${prometheus_folder}" ]]; then
+    echo "prometheus git repo not found! It is expected to exist here: ${prometheus_folder}"
+    echo "Clone the prometheus git repo and try again!"
+    #TODO: offer checkout
+    return
+  fi
+  pushd "${prometheus_folder}" > /dev/null
+    ./fix_jenkins_targets.sh
+    echo "TODO: Commit and push changes in ${prometheus_folder}"
+    #git_commit_push "${prometheus_folder}"
+  popd > /dev/null
+}
+
+
 
 # Main
 
@@ -186,9 +273,15 @@ _question_action "copy the backup to the archive" copy_backup_to_archive
 
 _question_action "delete the ${PROJECT_NAME} project on the cluster and in Jiro" delete_project
 
+_question_action "commit and push the changes to the Jiro repo" commit_jiro_changes
+
+_question_action "delete credentials from pass" delete_pass_credentials
+
+_question_action "update list of JIPPs on ci.eclipse.org" update_list_of_jipps
+
+_question_action "update prometheus scrape targets" update_prometheus_scrape_targets
+
+
 echo
 echo "TODO:"
-echo "- commit and push changes to jiro repo"
-echo "- comment on issue"
-echo "- remove credentials from pass"
-echo "- update ci.eclipse.org dashboard"
+echo "- comment on HelpDesk issue"
